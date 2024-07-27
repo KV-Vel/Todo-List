a2f465ec97d4c621f57adeb7cac7885a1ffa7b70
SprngVel <kraevvalentin98@gmail.com>
2024-07-27 13:00:26 +1000

    added initial design

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import './css/reset-css.css';
 import './css/style.css';
+
+const burgerMenuInput = document.querySelector('input');
+const header = document.querySelector('header');
+
+burgerMenuInput.addEventListener('change', () => {
+  header.classList.toggle('shrink-menu');
+});
