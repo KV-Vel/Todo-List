6125e3e0ffb6ee636a2bcef16eed2d7be7d980ae
SprngVel <kraevvalentin98@gmail.com>
2024-08-02 22:15:53 +1000

    added interactiveness to app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/reset-css.css';
 import './css/style.css';
 
+/* Header slide left and right  */
 const burgerMenuInput = document.querySelector('input');
 const header = document.querySelector('header');
 
@@ -8,6 +9,7 @@ burgerMenuInput.addEventListener('change', () => {
   header.classList.toggle('shrink-menu');
 });
 
+/* Add task form expand */
 const addTaskBtn = document.querySelector('.add-task-btn');
 const formWrapper = document.querySelector('.form-wrapper');
 
@@ -16,3 +18,40 @@ function expandForm() {
 }
 
 addTaskBtn.addEventListener('click', expandForm);
+
+/* Dialog add project, close and open */
+const addProjectBtn = document.querySelector('.add-project-btn');
+const dialog = document.querySelector('dialog');
+
+function showDialog() {
+  dialog.showModal();
+}
+
+const modalCloseBtn = document.querySelector('.close-modal');
+
+function closeModal() {
+  dialog.close();
+}
+
+modalCloseBtn.addEventListener('click', closeModal);
+addProjectBtn.addEventListener('click', showDialog);
+
+/* Project list show/hide */
+const showProjectsBtn = document.querySelector('.show-projects-list');
+const projectsList = document.querySelector('.projects-list');
+
+function hideProjects() {
+  projectsList.classList.toggle('hidden');
+}
+
+showProjectsBtn.addEventListener('click', hideProjects);
+
+/* Task expand/shrink */
+const hiddenPartOfTask = document.querySelector('.hidden-part');
+const showMoreBtn = document.querySelector('.task-show-more-btn');
+
+function toggleTaskDetails() {
+  hiddenPartOfTask.classList.toggle('active');
+}
+
+showMoreBtn.addEventListener('click', toggleTaskDetails);
