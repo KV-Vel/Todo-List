863a3803b11d5737b50fe515278fd6e85cb81eed
SprngVel <kraevvalentin98@gmail.com>
2024-08-04 12:06:29 +1000

    logic for adding, deleting subtask

--- src/js-modules/eventListeners.js
+++ src/js-modules/eventListeners.js
@@ -1,3 +1,6 @@
+import { className } from 'postcss-selector-parser';
+import createEl from './createElement';
+
 /* Header slide left and right  */
 const burgerMenuInput = document.querySelector('input');
 const header = document.querySelector('header');
@@ -56,8 +59,51 @@ showMoreBtn.addEventListener('click', toggleTaskDetails);
 /* Add subtask to form */
 const subTaskButton = document.querySelector('.subtask-add-btn');
 
-function createSubTask() {
+/* Clear subtask after adding */
+function clearSubTaskInput(input) {
+  input = '';
+  return input;
+}
 
+function createSubTask() {
+  const subTaskGroup = document.querySelector('.subtask-group');
+  const subtaskTextArea = document.querySelector('.subtask-textarea');
+
+  const subtaskDiv = createEl('div', {
+    className: 'subtask',
+  });
+
+  const subtaskPara = createEl('p', {
+    textContent: `${subtaskTextArea.value}`,
+  });
+
+  const deleteSubtaskBtn = createEl('button', {
+    type: 'button',
+  });
+
+  const crossIcon = createEl('i', {
+    className: 'fa-solid fa-xmark',
+  });
+
+  deleteSubtaskBtn.append(crossIcon);
+
+  subtaskDiv.append(subtaskPara, deleteSubtaskBtn);
+
+  subTaskGroup.append(subtaskDiv);
+
+  subtaskTextArea.value = clearSubTaskInput(subtaskTextArea.value);
 }
 
 subTaskButton.addEventListener('click', createSubTask);
+
+/* Delete subtask btn */
+const subtaskGroup = document.querySelector('.subtask-group');
+
+function deleteSubtask(e) {
+  const subtask = e.target.parentNode.parentNode;
+  if (e.target.classList.contains('fa-xmark')) {
+    subtask.remove();
+  }
+}
+
+subtaskGroup.addEventListener('click', deleteSubtask);
