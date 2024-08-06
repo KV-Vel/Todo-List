071b74bd68dbbfc11222ef842d767e34228a9c30
SprngVel <kraevvalentin98@gmail.com>
2024-08-06 13:46:59 +1000

    removed and changed name for this file

--- src/js-modules/create-task.js
+++
@@ -1,9 +0,0 @@
-export default class Task {
-  constructor(title, description, deadline, priority, subtasks = []) {
-    this.title = title;
-    this.description = 'Empty' || description;
-    this.deadline = deadline;
-    this.priority = priority;
-    this.subtasks = 'Empty' || subtasks;
-  }
-}
--- src/js-modules/createElement.js
+++
@@ -1,7 +0,0 @@
-export default function createEl(tag, properties) {
-  const el = document.createElement(tag);
-  for (const key in properties) {
-    el[key] = properties[key];
-  }
-  return el;
-}
